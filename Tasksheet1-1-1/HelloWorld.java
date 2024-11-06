3be145a90b4d6fea816dbeab3736e3674d8c26cd
Rodcel Lipana <134578071+rodcel2512@users.noreply.github.com>
2024-11-06 08:39:09 +0000

    Done task 1

---
+++ Tasksheet1-1-1/HelloWorld.java
@@ -0,0 +1,9 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorld { // clas helloWorldApp
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // System.out.rintln("Hello World!);
+}
+}
