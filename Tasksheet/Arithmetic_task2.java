b63ff5e995ac68b52af2d2b242587f41ba76d886
Rodcel Lipana <134578071+rodcel2512@users.noreply.github.com>
2024-11-07 11:17:20 +0000

    Update files

--- Tasksheet/Arithmetic_task2.java
+++ Tasksheet/Arithmetic_task2.java
@@ -1,4 +1,7 @@
-class ArithmeticTask {
+package Tasksheet;
+
+
+class Arithmetic_task2 {
 
     public static void main (String[] args){
           
--- Tasksheet/HelloWorld_task1.java
+++ Tasksheet/HelloWorld_task1.java
@@ -2,7 +2,7 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-class HelloWorld { // clas helloWorldApp
+class HelloWorld_task1 { // clas helloWorldApp
     public static void main(String[] args) {
         System.out.println("Kamusta Mundo!"); // System.out.rintln("Hello World!);
 }
