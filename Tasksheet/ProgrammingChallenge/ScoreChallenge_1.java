cdc2227fd53a1faa9ca4e602003749e03a772a8e
Rodcel Lipana <134578071+rodcel2512@users.noreply.github.com>
2024-11-22 01:57:54 +0000

    Done programming challenge 1

---
+++ Tasksheet/ProgrammingChallenge/ScoreChallenge_1.java
@@ -0,0 +1,28 @@
+package ProgrammingChallenge;
+
+public class ScoreChallenge_1 {
+    public static void main(String[] args) {
+        int score;
+        score = 85;
+
+        if (score >= 90) {
+            System.out.println("Grade A");
+        }
+        else if (score >= 80) {
+            System.out.println("Grabe B");
+        }
+        else if (score >= 70) {
+            System.out.println("Grade C");
+        }
+        else if (score >= 60) {
+            System.out.println("Grade D");
+        }
+        else if (score <= 59) {
+            System.out.println("Grade F");
+        }
+        else{
+            System.out.println("Not valid input!");
+        }
+    }
+
+}
