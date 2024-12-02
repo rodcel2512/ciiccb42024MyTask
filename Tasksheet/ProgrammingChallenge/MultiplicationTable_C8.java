9100ced99f36577daf4dc665e080a1cf4595404d
Rodcel Lipana <134578071+rodcel2512@users.noreply.github.com>
2024-12-02 12:48:57 +0000

    Done programming challenge 8

---
+++ Tasksheet/ProgrammingChallenge/MultiplicationTable_C8.java
@@ -0,0 +1,16 @@
+package ProgrammingChallenge;
+
+public class MultiplicationTable_C8 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        for(int i = 0; i <= num; i++){
+            for(int j = 0; j<=num; j++){
+                System.out.print(i*j + "\t");     
+            }
+        System.out.println();
+        }
+        
+    }
+
+}
